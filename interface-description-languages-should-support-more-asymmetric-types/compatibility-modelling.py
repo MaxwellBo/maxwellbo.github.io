8644904bc4a184f032b0702830ada431d50304a7
Max Bo <max@maxbo.me>
2026-02-22 08:03:45 -0800

    More updates

---
+++ interface-description-languages-should-support-more-asymmetric-types/compatibility-modelling.py
@@ -0,0 +1,31 @@
+
+
+class SchemaVersion(object):
+
+def check_proposal(
+  client_schema_versions: List[SchemaVersion], 
+  server_schema_versions: List[SchemaVersion],
+  proposed_schema_version: SchemaVersion):
+
+
+  for client_schema_version:
+
+
+
+
+
+  pass
+
+
+
+
+def check_schema(client_schema_version: SchemaVersion, servesr_schema_version: SchemaVersion):
+  pass
+
+
+
+
+# [ CLIENT v0, CLIENT V1, PROPOSED ] 
+#             X          X          X 
+#             X          X          X 
+# [ SERVER v0, SERVER V1, PROPOSED]
